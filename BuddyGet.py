a09a8c0c3b2897f4f9032bd8c4ff65a46018569f
BrtKST <b.kostuj1011@gmail.com>
2026-01-07 20:04:49 +0100

    Create first version
    The first version of my simple program. Updates will include some features, such as using an SQL datasheet, add your own category

---
+++ BuddyGet.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# 1. Tytuł aplikacji
+st.title("BuddGet - asystent twoich finansów💰")
+# 2. Tworzymy przykładowe dane (zamiast Excela)
+if 'wydatki' not in st.session_state:
+    st.session_state['wydatki'] = {
+        'Jedzenie': 0,
+        'Transport': 0,
+        'Czynsz': 0,
+        'Rozrywka': 0,
+        'Nauka': 0,
+        'Inne': 0
+    }
+
+# Panel boczny - tutaj dodajemy nowy produkt
+st.sidebar.header('Dodaj nowy wydatek')
+# Pobieramy kategorie z kluczy naszego słownika
+kategorie_lista = list(st.session_state['wydatki'].keys())
+new_category = st.sidebar.selectbox('Kategoria', kategorie_lista)
+new_price = st.sidebar.number_input('Kwota [PLN]', min_value=0, value=50)
+
+if st.sidebar.button('Dodaj do listy'):
+    st.sidebar.success(f'Dodano: {new_category} - {new_price} PLN')
+    st.session_state['wydatki'][new_category] += new_price
+    #potem dodać baze danych i przekazanie wartości
+        
+
+
+# Zamiana danych na tabelę (DataFrame)
+to_table = list(st.session_state['wydatki'].items())
+df = pd.DataFrame(to_table, columns=['Kategoria', 'Kwota [PLN]'])
+
+
+
+col1, col2 = st.columns(2, vertical_alignment = 'center') #podział na 2 kolumny, wyśrodkowanie
+
+# Wyświetlamy prosty wykres słupkowy
+with col1:
+    st.write("Tabela z wydatakmi:")
+    st.dataframe(df)
+with col2:
+    st.write('Wykres:')
+    if df['Kwota [PLN]'].sum() > 0:
+        fig = px.pie(df, values='Kwota [PLN]', names='Kategoria')
+        st.plotly_chart(fig,use_container_width=True)
+    else:
+        st.info('Aby wyświetlić wykres dodaj pierwszy wydatek')
+
+# 5. Dodatek: Szybkie podsumowanie
+suma = df['Kwota [PLN]'].sum()
+st.metric(label="Łączne wydatki", value=f"{suma} PLN")
+
+if suma > 2000:
+    st.warning("Uwaga! Przekroczyłeś budżet 2000 zł!")
+else:
+    st.success("Budżet pod kontrolą.")
